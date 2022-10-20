b5f6dd3cd8310fe071a77075acb5353ddcd5a99c
EdenHGiorgis <edenhgiorgis21@gmail.com>
2022-10-20 21:23:18 +0300

    model class:employee

--- RealEstateManagmentSystem/RealEstateManagmentSystem/Models/Employee.cs
+++ RealEstateManagmentSystem/RealEstateManagmentSystem/Models/Employee.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace RealEstateManagmentSystem.Models
 {
-    public partial class Employee : Form
+    internal class Employee
     {
-        public Employee()
-        {
-            InitializeComponent();
-        }
     }
 }
